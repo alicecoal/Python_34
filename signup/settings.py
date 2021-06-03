234e0850632968ebbe37de19b68f6713d33b6877
alicecoal <anna.goncharova_2002@mail.ru>
2021-06-04 02:26:20 +0300

    some fixed interface

--- signup/settings.py
+++ signup/settings.py
@@ -128,4 +128,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
