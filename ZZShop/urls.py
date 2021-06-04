f86e8c38338d54c693e3d6d179a87bf5308c4f28
alicecoal <anna.goncharova_2002@mail.ru>
2021-06-04 03:37:13 +0300

    fixed interface

--- ZZShop/urls.py
+++ ZZShop/urls.py
@@ -26,12 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),  # админкая страничка
     path('', home_view, name="index"),  # index
     path('signup/', signup_view, name="signup"),  # страничка регистрации
-    path('sent/', activation_sent_view, name="activation_sent"),  # отправка писем
-    path('activate/<slug:uidb64>/<slug:token>/', activate, name='activate'),  # активация писем при регистрации
     url(r'^account/', include('accounts.urls')),  # для аккаунтов
     path('shop/', include(('shop.urls', 'shop'), namespace="shop")),  # для магазинов
     path('wallet/', include('wallets.urls')),  # для кошельков
-    url('social-auth/', include('social.apps.django_app.urls', namespace='social')),  # авторизация через соцсети
 ]
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # просмотр медиа файлов
